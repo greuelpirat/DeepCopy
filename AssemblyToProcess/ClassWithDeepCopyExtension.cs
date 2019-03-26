67a11b35ba199aaeccb48f2878dae31df600c6e3
Oliver Ehrenmüller <oliver.ehrenmueller@gmail.com>
2019-03-26 06:43:27 +0100

    CopySomeObject

--- AssemblyToProcess/ClassWithDeepCopyExtension.cs
+++ AssemblyToProcess/ClassWithDeepCopyExtension.cs
@@ -5,6 +5,6 @@ namespace AssemblyToProcess
     public static class ClassWithDeepCopyExtension
     {
         [DeepCopyExtension]
-        public static SomeObject CopyOtherObject(SomeObject source) => source;
+        public static SomeObject CopySomeObject(SomeObject source) => source;
     }
 }
--- Tests/DeepCopyExtensionTests.cs
+++ Tests/DeepCopyExtensionTests.cs
@@ -7,12 +7,12 @@ namespace Tests
     public partial class WeaverTests
     {
         [Fact]
-        public void TestSampleDeepCopyProvider()
+        public void TestClassWithDeepCopyExtension()
         {
             var instance = CreateSomeObject();
 
             var method = GetTestType(typeof(ClassWithDeepCopyExtension))
-                .GetMethod(nameof(ClassWithDeepCopyExtension.CopyOtherObject), BindingFlags.Public | BindingFlags.Static);
+                .GetMethod(nameof(ClassWithDeepCopyExtension.CopySomeObject), BindingFlags.Public | BindingFlags.Static);
             Assert.NotNull(method);
 
             dynamic copy = method.Invoke(null, new object[] { instance });
