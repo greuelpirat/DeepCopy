40cabd1df46b1a81eb3ee743aa0471a6c42e1ec0
Oliver Ehrenmüller <oliver.ehrenmueller@gmail.com>
2020-08-24 19:55:46 +0200

    Overwrite TypeSystem

--- DeepCopy.Fody/ModuleWeaver.cs
+++ DeepCopy.Fody/ModuleWeaver.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using TypeSystem = Fody.TypeSystem;
 
 namespace DeepCopy.Fody
 {
@@ -28,6 +29,8 @@ namespace DeepCopy.Fody
 
         public override void Execute()
         {
+            TypeSystem = new TypeSystem(name => ImportType(Type.GetType(name)).Resolve(), ModuleDefinition);
+            
             CreateDeepCopyExtensions();
             AddDeepCopyConstructors(AddDeepCopyConstructorTargets.Values, false);
             AddDeepCopyConstructors(ModuleDefinition.GetTypes().Where(t => t.AnyAttribute(AddDeepCopyConstructorAttribute)), true);
